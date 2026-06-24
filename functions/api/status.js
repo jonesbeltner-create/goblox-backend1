a68cf98087274327f103ec739848b4289494bac3
jonesbeltner-create <jonesbeltner@gmail.com>
2026-06-24 11:56:49 -0400

    Update status.js

--- functions/api/status.js
+++ functions/api/status.js
@@ -1,1 +1,16 @@
+export async function onRequest(context) {
+  const gobloxData = {
+    server: "GoBlox API Gateway",
+    status: "Active",
+    engine: "Godot Engine",
+    avatar_style: "R6 Classic"
+  };
+
+  return new Response(JSON.stringify(gobloxData), {
+    headers: { 
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*" 
+    }
+  });
+}
 
